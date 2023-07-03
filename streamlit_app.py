7e2e941e87a23e8810c6783112cc0044aa386226
Carlos Sánchez <92407304+CarlitosOMG@users.noreply.github.com>
2023-07-03 10:32:01 -0600

    Update streamlit_app.py
    Edit file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit 
 
+streamlit.title('this is my new app')
+
+streamlit.header('Take a breakfast')
+streamlit.text('this is a text')
+streamlit.text('this is a text')
+streamlit.text('this is a text')
