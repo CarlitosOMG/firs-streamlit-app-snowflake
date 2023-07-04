d4bee3b7f7a6be5e3777f3adde5dcd180d29aa66
Carlos Sánchez <92407304+CarlitosOMG@users.noreply.github.com>
2023-07-03 18:31:17 -0600

    Update streamlit_app.py
    Normalizeed data of the API with pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# Normalize the data with pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Show the new data normalized
+streamlit.dataframe(fruityvice_normalized)
